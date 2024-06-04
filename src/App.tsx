5af8f999c74569354c5659510b84cf5e79c40e70
moha <m.saket1990@gmail.com>
2024-06-04 17:00:43 +0330

    +++

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,10 @@ const App: React.FC = () => {
     <div className="App font-custom ">
       {!showTimer ? <WelcomePage /> : <PomodoroTimer />}
       {!showTimer && (
-        <div className=" fixed  w-full h-full flex text-center items-center justify-center ">
+        <div className="butty">
         <button
           onClick={handleStartTimer}
-          className=" flex items-center justify-center top-[-250px] px-4 py-2 font-bold bg-[#ffcbcb] text-[#cb5b60] rounded top-20 left-20 right-20 bottom-20 absolute  w-[200px] h-[48px]"
+          className="  px-4 py-2 font-bold bg-[#ffcbcb] text-[#cb5b60] rounded w-[200px] h-[48px]"
         >
    شروع زمان
         </button>
